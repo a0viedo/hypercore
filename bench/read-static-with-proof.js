352f635c3af43c43376a26d0c07ec4259e0e69f6
Mathias Buus <mathiasbuus@gmail.com>
2016-11-23 17:34:04 -0300

    update example, add benchs

---
+++ bench/read-static-with-proof.js
@@ -0,0 +1,31 @@
+var raf = require('random-access-file')
+var explain = require('explain-error')
+var through = require('through2')
+var hypercore = require('../')
+
+var feed = hypercore({live: false}, function (name) {
+  return raf(__dirname + '/static/' + name)
+})
+
+var then = Date.now()
+var size = 0
+var cnt = 0
+
+feed.createReadStream().pipe(through(write))
+  .on('data', function (data) {
+    size += data.length
+  })
+  .on('end', function () {
+    console.log(Math.floor(1000 * size / (Date.now() - then)) + ' bytes/s')
+    console.log(Math.floor(1000 * cnt / (Date.now() - then)) + ' blocks/s')
+  })
+
+function write (data, enc, cb) {
+  feed.proof(cnt++, function (err) {
+    if (err) return cb(err)
+    cb(null, data)
+  })
+}
+
+process.title = 'testme'
+setInterval(function () {}, 1000)
--- example.js
+++ example.js
@@ -6,86 +6,26 @@ var bulk = require('bulk-write-stream')
 var fs = require('fs')
 
 var then = Date.now()
-var w = hypercore(function (name) {
+var w = hypercore({valueEncoding: 'json'}, function (name) {
   return raf('tmp/' + name)
 })
 
-w.append(JSON.stringify({
+w.ready(function () {
+  console.log('Contains %d blocks and %d bytes\n', w.blocks, w.bytes)
+})
+
+w.createReadStream()
+  .on('data', console.log)
+  .on('end', console.log.bind(console, '\n(end)'))
+
+w.append({
   hello: 'world'
-}) + '\n')
+})
 
-w.append(JSON.stringify({
+w.append({
   hej: 'verden'
-}) + '\n')
+})
 
-w.append([JSON.stringify({
+w.append({
   hola: 'mundo'
-}) + '\n'])
-
-w.flush(function () {
-  console.log('??', w.blocks, w.bytes)
 })
-
-// w.open(function () {
-//   console.log('opened')
-// })
-
-return
-
-var rs = fs.createReadStream('/Users/maf/Downloads/tears_of_steel_1080p.webm')
-// var rs = fs.createReadStream('/Users/maf/Downloads/win7.iso')
-// var rs = fs.createReadStream('/Users/maf/Downloads/video.webm')
-// var rs = fs.createReadStream(__filename)
-var ws = w.createWriteStream()
-var total = 0
-
-rs.pipe(ws).on('finish', function () {
-  console.log('bytes/ms', w.bytes / (Date.now() - then))
-  console.log('blocks', w.blocks)
-
-  w.finalize(function () {
-    console.log('key', w.key.toString('hex'))
-    // return
-    var r = hypercore(w.key, function (name) {
-      return raf('/tmp/hypercore-clone/' + name)
-    })
-
-
-    // copy(w, r, 536, console.log)
-
-    // return
-
-    var missing = []
-    for (var i = 0; i < w.blocks; i++) missing.push(i)
-    missing = require('shuffle-array')(missing)
-
-    var then = Date.now()
-
-    loop()
-
-    function loop (err) {
-      if (err) throw err
-      if (!missing.length) return done()
-      copy(w, r, missing.shift(), loop)
-    }
-
-    function done () {
-      console.log('done!')
-      console.log('bytes/ms', w.bytes / (Date.now() - then))
-      console.log('from: %d blocks, %d bytes', w.blocks, w.bytes)
-      console.log('to: %d blocks, %d bytes', r.blocks, r.bytes)
-    }
-  })
-})
-
-function copy (from, to, index, cb) {
-  from.proof(index, {tree: to.tree}, function (err, proof) {
-    if (err) return cb(err)
-    total += proof.nodes.length
-    // console.log('index: %d, proof -> {nodes: %d, signature: %s}, total-nodes: %d', index, proof.nodes.length, !!proof.signature, total)
-    from.get(index, function (err, data) {
-      if (err) return cb(err)
-      to.put(index, data, proof, cb)
-    })
-  })
-}
